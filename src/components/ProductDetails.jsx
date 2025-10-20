76180ae5623fa65d248d4f967d441321939e2e18
Hossain Forhad <hossain.28forhad@gmail.com>
2025-10-20 16:32:36 +0600

    Show single product from fetch api

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,22 +1,58 @@
-import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
+
 
 const ProductDetails = () => {
-    const {state} = useLocation();
-    // console.log(state);
+  const { state } = useLocation();
+  // console.log(state);
+  const [product, setProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const { id } = useParams();
+
+
+  const fetchData = () => {
+    setIsLoading(true);
+    setError(null);
+
+    fetch(`https://dummyjson.com/products/${id}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Data could not be fetched');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setProduct(data);
+        // console.log(data);
+        setIsLoading(false);
+      })
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="product-details">
       <h2>Product Details</h2>
-      {state ? <article className="product-details-article">
+      {isLoading && <p>Product are loading...</p>}
+      {error && <p>Error</p>}
+      {!isLoading && !error && (
+        <article className="product-details-article">
         <img src={state.images[0]} alt={state.title} className="product-details-img" />
         <h3 className="product-details-title">{state.title}</h3>
-              <p><strong>Category: </strong> {state.category}</p>
-              <p><strong>Price:</strong> {state.price}</p>
-              <p><strong>Rating:</strong> {state.rating}</p>
-              <p><strong>Brand:</strong> {state.brand}</p>
-              <p><strong>Description:</strong> {state.description && state.description.substring(1,100)}...</p>
-              <Link to={`/products`} className="product-link">Continue Shopping</Link>
-      </article> : <p>Product details not available</p>
-      }
+        <p><strong>Category: </strong> {state.category}</p>
+        <p><strong>Price:</strong> {state.price}</p>
+        <p><strong>Rating:</strong> {state.rating}</p>
+        <p><strong>Brand:</strong> {state.brand}</p>
+        <p><strong>Description:</strong> {state.description && state.description.substring(1, 100)}...</p>
+        <Link to={`/products`} className="product-link">Continue Shopping</Link>
+      </article>
+      )}
+
     </div>
   )
 }
